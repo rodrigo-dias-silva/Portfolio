2679f23587105a1d4a62448422a16ad4622653f8
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-03-11 13:58:44 -0300

    fix: atualizaçao link cantact section

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -15,9 +15,9 @@ export default function ContactSection() {
             Vamos trabalhar juntos!
           </span>
           <ul className="flex gap-5">
-            <li><a href="https://bit.ly/3xy2RXc" className="hover:text-teal-500"><WhatsappLogo size={50} weight="thin" /></a></li>
-            <li><a href="https://bit.ly/3MdLzFi" className="hover:text-teal-500"><LinkedinLogo size={50} weight="thin" /></a></li>
-            <li><a href="mailto:diglesilva@icloud.com" className="hover:text-teal-500"><Envelope size={50} weight="thin" /></a></li>
+            <li><a href="https://bit.ly/3xy2RXc" target="_blank" className="hover:text-teal-500"><WhatsappLogo size={50} weight="thin" /></a></li>
+            <li><a href="https://bit.ly/3MdLzFi" target="_blank" className="hover:text-teal-500"><LinkedinLogo size={50} weight="thin" /></a></li>
+            <li><a href="mailto:diglesilva@icloud.com" target="_blank" className="hover:text-teal-500"><Envelope size={50} weight="thin" /></a></li>
           </ul>
         </div>
       </div>
